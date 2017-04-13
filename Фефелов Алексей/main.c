edcbb3f19008a332b6e9dc584483d33542ac559d
Aleksey Fefelov <fefaleksey@mail.ru>
2017-04-13 17:00:34 +0300

    Ok

--- Фефелов Алексей/main.c
+++ Фефелов Алексей/main.c
@@ -9,7 +9,10 @@
 int main()
 {
     FILE *f1,*f2;
-    f1=fopen("input.txt","r");
+    char a[100];
+    fgets(a,100,stdin);
+    if(a[strlen(a)-1]=='\n') a[strlen(a)-1]='\0';
+    f1=fopen(a,"r");
     f2=fopen("output.txt","w");
     program(f1,f2);
     test();
